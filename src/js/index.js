ba304410ae21eb4342f26e2c5d737027cb766ed0
alston chan <alston.chan22@sitechhs.com>
2021-05-10 03:30:43 -0400

    coingecko

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
+//1. Import coingecko-api
+const CoinGecko = require("coingecko-api");
 
-const key = "YOURKEYHERE";
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+var func = async () => {
+  let data = await CoinGeckoClient.ping();
+};
+
+let test = await CoinGeckoClient.coins.all();
+
+console.log(test);
