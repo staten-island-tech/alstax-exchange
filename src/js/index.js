704d81aed831f36c6b844b27656431aacdd2282a
alston chan <alston.chan22@sitechhs.com>
2021-05-14 03:31:33 -0400

    basic api setup done

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,37 @@
-//1. Import coingecko-api
-const CoinGecko = require("coingecko-api");
+let apiLink = `https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`;
+document.getElementById("search-button").addEventListener("click", search);
 
-//2. Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
+function search() {
+  const userSearch = document.getElementById("input").value;
+  apiLink = `https://api.coingecko.com/api/v3/coins/${userSearch}?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`;
+  query();
+}
 
-//3. Make calls
-var func = async () => {
-  let data = await CoinGeckoClient.ping();
+const query = async function () {
+  try {
+    const response = await fetch(apiLink);
+    const data = await response.json();
+    console.log(data);
+    const coinName = data.name;
+    const coinTicker = data.symbol;
+    const coinTickerUp = coinTicker.toUpperCase();
+    const coinPrice = data.market_data.current_price.usd;
+    const coinImage = data.image.large;
+    const coinChange24h = data.market_data.price_change_percentage_24h;
+    const coinChange7d = data.market_data.price_change_percentage_7d;
+
+    document.getElementById("image").src = `${coinImage}`;
+    document.getElementById("name").innerText = `Name: ${coinName}`;
+    document.getElementById("ticker").innerText = `Ticker: ${coinTickerUp}`;
+    document.getElementById("price").innerText = `Current Price: $${coinPrice}`;
+    document.getElementById(
+      "daily"
+    ).innerText = `24hrs % Change: ${coinChange24h}%`;
+    document.getElementById(
+      "weekly"
+    ).innerText = `7d % Change: ${coinChange7d}%`;
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-let test = await CoinGeckoClient.coins.all();
-
-console.log(test);
+query();
