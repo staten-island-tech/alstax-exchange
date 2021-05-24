7cfa7684bda65d7ca27e6f4f93f2a315c5467015
alston chan <alston.chan22@sitechhs.com>
2021-05-24 19:19:09 -0400

    Revert "basic api setup done"
    This reverts commit 704d81aed831f36c6b844b27656431aacdd2282a.

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,14 @@
-let apiLink = `https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`;
-document.getElementById("search-button").addEventListener("click", search);
+//1. Import coingecko-api
+const CoinGecko = require("coingecko-api");
 
-function search() {
-  const userSearch = document.getElementById("input").value;
-  apiLink = `https://api.coingecko.com/api/v3/coins/${userSearch}?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`;
-  query();
-}
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko();
 
-const query = async function () {
-  try {
-    const response = await fetch(apiLink);
-    const data = await response.json();
-    console.log(data);
-    const coinName = data.name;
-    const coinTicker = data.symbol;
-    const coinTickerUp = coinTicker.toUpperCase();
-    const coinPrice = data.market_data.current_price.usd;
-    const coinImage = data.image.large;
-    const coinChange24h = data.market_data.price_change_percentage_24h;
-    const coinChange7d = data.market_data.price_change_percentage_7d;
-
-    document.getElementById("image").src = `${coinImage}`;
-    document.getElementById("name").innerText = `Name: ${coinName}`;
-    document.getElementById("ticker").innerText = `Ticker: ${coinTickerUp}`;
-    document.getElementById("price").innerText = `Current Price: $${coinPrice}`;
-    document.getElementById(
-      "daily"
-    ).innerText = `24hrs % Change: ${coinChange24h}%`;
-    document.getElementById(
-      "weekly"
-    ).innerText = `7d % Change: ${coinChange7d}%`;
-  } catch (error) {
-    console.log(error);
-  }
+//3. Make calls
+var func = async () => {
+  let data = await CoinGeckoClient.ping();
 };
-query();
+
+let test = await CoinGeckoClient.coins.all();
+
+console.log(test);
