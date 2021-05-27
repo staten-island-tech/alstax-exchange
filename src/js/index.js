4062c70f276c3e4bd01d3da258b2adf9884becca
alston chan <alston.chan22@sitechhs.com>
2021-05-26 22:44:40 -0400

    more things

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-let apiLink = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`;
+let apiLink = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250`;
 
 const query = async function () {
   try {
@@ -7,7 +7,10 @@ const query = async function () {
     console.log(data);
 
     let coinNumber = 0;
-    data.forEach((coin) => {
+    data.every((coin) => {
+        if (coinNumber >= 100) {
+            return false;
+        }
         const eachCoin = data[coinNumber];
         coinNumber++;
 
@@ -15,27 +18,35 @@ const query = async function () {
         const symbol = eachCoin.symbol;
         const symbolUpper = symbol.toUpperCase();
         const price = eachCoin.current_price;
-        const coinImage = eachCoin.image.large;
+        const coinImage = eachCoin.image;
         const coinChange24h = eachCoin.price_change_percentage_24h;
-        const coinChange7d = eachCoin.price_change_percentage_7d;
+        const coinChange24hDollars = eachCoin.price_change_24h;
         const marketCap = eachCoin.market_cap;
         const volume = eachCoin.total_volume;
-        const supply = eachCoin.total_supply;
+        const supply = eachCoin.circulating_supply;
 
         const coinHTML = document.getElementById("coin-table");
         coinHTML.insertAdjacentHTML("beforeend", `
             <div class="bg-gray-600 text-left p-4">${coinNumber}</div>
-            <div class="bg-gray-600 text-left p-4">${name} - ${symbolUpper}</div>
+            <div class="bg-gray-600 text-left p-4 font-semibold"><img class="max-h-4 m-0 pr-1 inline" src="${coinImage}" /> ${name} (${symbolUpper})</div>
             <div class="bg-gray-600 text-left p-4">$${price}</div>
-            <div class="bg-gray-600 text-left p-4">${coinChange24h}</div>
-            <div class="bg-gray-600 text-left p-4">${coinChange7d}</div>
-            <div class="bg-gray-600 text-left p-4">${marketCap}</div>
-            <div class="bg-gray-600 text-left p-4">${volume}</div>
-            <div class="bg-gray-600 text-left p-4">${supply}</div>
+            <div class="bg-gray-600 text-left p-4">${coinChange24h}%</div>
+            <div class="bg-gray-600 text-left p-4">$${coinChange24hDollars}</div>
+            <div class="bg-gray-600 text-left p-4">$${marketCap}</div>
+            <div class="bg-gray-600 text-left p-4">$${volume}</div>
+            <div class="bg-gray-600 text-left p-4">${supply} ${symbolUpper}</div>
         `);
+        return true;
     })
   } catch (error) {
     console.log(error);
   }
 };
 query();
+
+
+
+/* searchInput.addEventListener("onkeyup", event => {
+    myFunction();
+}); */
+
