0ef4d26b0b380f3d987705add0f99f58ba9d2d18
alston chan <alston.chan22@sitechhs.com>
2021-05-26 21:17:51 -0400

    stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let apiLink = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`;
+
+const query = async function () {
+  try {
+    const response = await fetch(apiLink);
+    const data = await response.json();
+    console.log(data);
+
+    let coinNumber = 0;
+    data.forEach((coin) => {
+        console.log(data[coinNumber]);
+        coinNumber++;
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
+query();
